e6f32185136e6c18de9a0e63e7e8f43fdd5a339b
SkylarkTechTeam <skylarktechco@gmail.com>
2016-01-23 19:51:06 -0500

    initial commit Social App chap 8

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ActiveRecord::Base
+  belongs_to :post
+  belongs_to :user
+
+  validates :post_id, presence: true
+  validates :user_id, presence: true
+  #validates :body, presence: true
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ActiveRecord::Base
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :type, presence: true
+end
