78db8d194a4ea945284cc0024fe292fc70a51ff4
SkylarkTechTeam <skylarktechco@gmail.com>
2016-01-24 19:18:40 -0500

    authentication - working end of exercises

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user!
+
+	def create
+    	@comment = current_user.comments.build(comment_params)
+   	 if @comment.save
+   	 	
+     	 redirect_to post_path(@comment.post_id),
+                  notice: 'Comment was successfully created.'
+   	 else
+      redirect_to post_path(@comment.post_id),
+                  alert: 'Error creating comment.'
+    end
+  end
+
 end
