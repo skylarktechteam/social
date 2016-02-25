9b10038e4939f260e36657bcaf25387501123c0b
SkylarkTechTeam <skylarktechco@gmail.com>
2016-02-24 22:42:25 -0500

    Bye Bug logs debugger

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,24 @@ class PostsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
+
+		logger.debug "** current_user.inspect = "
+		logger.debug current_user.inspect
 		  user_ids = current_user.timeline_user_ids
+
+		  debugger
+
    			@posts = Post.includes(:user).where(user_id: user_ids)
    			.paginate(page: params[:page], per_page: 5)
    			.order("created_at DESC")
+
+   		@posts.each do |post|
+        logger.debug "Post #{post.id} is a #{post.type}"
+		end
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		@post = Post.includes(comments: [:user]).find(params[:id])
 		@can_moderate = (current_user == @post.user)
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		debugger
 	    user = User.find_by(email: params[:email])
 		    if user && user.authenticate(params[:password])
 		      session[:user_id] = user.id
